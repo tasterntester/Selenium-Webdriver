cca29fd82eb82807e2eadc2021121776d906382c
Nitesh Ghorpade <88904226+niteshghorpade@users.noreply.github.com>
2021-09-01 02:42:05 +0530

    Update OpenUrlProgram.java

--- OpenUrlProgram.java
+++ OpenUrlProgram.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenUrlProgram {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
 		 
 		// Setting the property of chrome browser and passing chromedriver path 
 		System.setProperty("webdriver.chrome.driver","D:\\software\\chromedriver_win32 (2)\\chromedriver.exe");
